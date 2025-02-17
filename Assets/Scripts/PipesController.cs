a07b92fa50c493b747a292dfc98dfbbd419e7263
pgsweet <113042887+pgsweet@users.noreply.github.com>
2025-02-17 14:43:04 -0500

    i got lost

--- Assets/Scripts/PipesController.cs
+++ Assets/Scripts/PipesController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PipesController : MonoBehaviour
 {
     public float Move_Speed = 5;
+    public int deadSpace = 40;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +15,9 @@ public class PipesController : MonoBehaviour
     void Update()
     {
         gameObject.transform.position += Vector3.left * Time.deltaTime * Move_Speed;
+        if (gameObject.transform.position.x < -deadSpace) 
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public GameObject Pipes_Prefab;
+    private float timer = 0.0f;
+    public float spawnTime = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,11 @@ public class SpawnController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < spawnTime) {
+            timer += Time.deltaTime;
+        } else {
+            Instantiate(Pipes_Prefab, transform.position, transform.rotation);
+            timer = 0.0f;
+        }
     }
 }
